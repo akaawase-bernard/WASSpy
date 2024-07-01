7e5640db46ae6f442bdfac8b56eafed24a55c6ee
Bernard Akaawase <89154021+akaawase-bernard@users.noreply.github.com>
2024-07-01 16:46:57 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="wasspy",
+    version="0.1",
+    py_modules=["wasspy"],
+    install_requires=[],
+    description="The fully python implementaion of Wave Acquisation and Stereo System (WASS)",
+    author="Bernard Akaawase",
+    author_email="akaawasebernard@gmail.com",
+    url="https://github.com/akaawase-bernard/wasspy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
